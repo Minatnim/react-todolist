6b69a4e764c54e5c57ca9fdfbd3191e12df296ff
minatnim <min.atnim@gmail.com>
2023-04-25 16:02:27 +0700

    addtodo

---
+++ src/components/Todo/TodoLists.jsx
@@ -0,0 +1,13 @@
+import { TodoItem } from "./TodoItem";
+
+export function TodoLists({ todos }) {
+  return (
+    <ul>
+      {todos.map((todoObj) => (
+        <TodoItem key={todoObj.id} todo={todoObj} />
+      ))}
+    </ul>
+  );
+}
+
+// {id:1, task: asdadsasdas, status : false, due_date : 2002-04-20}
